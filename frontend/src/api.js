18822887e28547d50a9a6c179bc9aad7d39c2150
personal github account <22f3001166@ds.study.iitm.ac.in>
2026-09-04 11:25:31 +0530

    Final Changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -13,7 +13,7 @@ import axios from "axios";
 
 // 1. Create a configured Axios instance
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://farmer-sih2026.onrender.com/",
   timeout: 10000, // 10 second timeout
   headers: {
     "Content-Type": "application/json",
